79e77beda55d38385125a562bcbce3b0d1359832
kiprotich267 <enockierui@gmail.com>
2025-05-22 16:45:23 +0300

    added app/models/student.py

--- app/models/student.py
+++ app/models/student.py
@@ -1,17 +1,19 @@
-from extension import db
+from app.extension import db
 from datetime import datetime, timezone
+from app.models.enrollment import EnrollmentModel
+from app.models.fee import FeeModel
 
 class StudentModel(db.Model):
     __tablename__ = 'students'
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.string(80), nullable=False)
-    last_name = db.Column(db.string(80), nullable=False)
-    student_id = db.Column(db.string(20), nullable=False, unique=True)
-    email = db.Column(db.string(120), nullable=False, unique=True)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    student_id = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
     date_of_birth = db.Column(db.Date)
-    enrolment_date = db.Column(db.DateTime, default=datetime.now(timezone.utc))
-    enrolments = db.relationship('EnrolmentModel', backref='student', lazy=True)
-    fees = db.relationship('FeeModel', backref='student', lazy=True)
+    enrollment_date = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    enrollments = db.relationship('EnrollmentModel', backref='student', lazy=True)
+    fees = db.relationship('FeeModel', backref='student_ref', lazy=True)
     
     def __repr__(self):
         return f"{self.student_id} {self.first_name} {self.last_name}"
