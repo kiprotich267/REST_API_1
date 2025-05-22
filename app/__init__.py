83913f71996ff1fcf1368182c3435553b6d9c96e
kiprotich267 <enockierui@gmail.com>
2025-05-22 16:38:41 +0300

    added init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask_restful import Api
 from app.resources.user import Users, user
+from app.resources.teacher import Teachers
 from app.extension import db
 from config import Config
 from flask_migrate import Migrate
@@ -19,3 +20,4 @@ migrate = Migrate(app, db)
 # Api endpoints
 api.add_resource(Users, '/api/users')
 api.add_resource(user, '/api/users/<int:id>')
+api.add_resource(Teachers, '/api/teachers')
