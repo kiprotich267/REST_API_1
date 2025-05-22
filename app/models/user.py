a311c998c0394b7acf0fc97e4337f9caa9a14e14
kiprotich267 <enockierui@gmail.com>
2025-05-22 09:03:56 +0300

    added app/models

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from app.extension import db
+
+
+
+#Database model
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f"{self.username} {self.email}"
