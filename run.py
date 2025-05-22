68b3aed9de6635532c2985521f96606ca578f5c4
kiprotich267 <enockierui@gmail.com>
2025-05-22 16:47:54 +0300

    modified run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from app import app, db
 
-
-
+with app.app_context():
+        db.create_all()
+        print("Tables created successfully")
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
