490cbf48854b9bfba7e199ea1a1f7b8ad273d7c2
kiprotich267 <enockierui@gmail.com>
2025-05-23 13:35:50 +0300

    modified app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from app.resources.user import Users, user
 from app.resources.teacher import Teachers, Teacher
+from app.resources.student import Students, Student
 from app.extension import db
 from config import Config
 from flask_migrate import Migrate
@@ -20,5 +21,9 @@ migrate = Migrate(app, db)
 # Api endpoints
 api.add_resource(Users, '/api/users')
 api.add_resource(user, '/api/users/<int:id>')
+
 api.add_resource(Teachers, '/api/teachers')
 api.add_resource(Teacher, '/api/teachers/<int:id>')
+
+api.add_resource(Students, '/api/students')
+api.add_resource(Student, '/api/students/<int:id>')
